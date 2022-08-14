94da34c95f50ebb846f47b3b757dbde6d9c583d0
TobiasLueger <tobias.lueger98@gmail.com>
2022-08-14 20:09:41 +0200

    [WIP] removed fonts from Outh

--- screens/Outh.js
+++ screens/Outh.js
@@ -15,20 +15,9 @@ import firebase from "firebase/compat/app";
 import LayoutWrapper from "../components/LayoutWrapper/LayoutWrapper";
 import {
   useFonts,
-  Rubik_300Light,
   Rubik_400Regular,
   Rubik_500Medium,
-  Rubik_600SemiBold,
   Rubik_700Bold,
-  Rubik_800ExtraBold,
-  Rubik_900Black,
-  Rubik_300Light_Italic,
-  Rubik_400Regular_Italic,
-  Rubik_500Medium_Italic,
-  Rubik_600SemiBold_Italic,
-  Rubik_700Bold_Italic,
-  Rubik_800ExtraBold_Italic,
-  Rubik_900Black_Italic,
 } from "@expo-google-fonts/rubik";
 
 const Outh = ({ navigation }) => {
